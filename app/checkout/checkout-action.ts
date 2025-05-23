f2cfb14ba6c645bc7f224c2e754c3993fe2492dc
rmandici <robertmandici29@gmail.com>
2025-05-23 16:15:27 +0300

    Changed BASE_URL

--- app/checkout/checkout-action.ts
+++ app/checkout/checkout-action.ts
@@ -20,8 +20,8 @@ export const checkoutAction = async (formData: FormData): Promise<void> => {
     payment_method_types: ["card"],
     line_items,
     mode: "payment",
-    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
-    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
+    success_url: `${process.env.BASE_URL}/success`,
+    cancel_url: `${process.env.BASE_URL}/checkout`,
   });
 
   redirect(session.url!);
